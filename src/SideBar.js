5039e32ebbc67ee390be5edf3395f38bb5cb8c20
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-06-03 00:22:15 -0600

    Completed base tutorial from fireship.io

---
+++ src/SideBar.js
@@ -0,0 +1,33 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
+const SideBar = () => {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 m-0
+                        flex flex-col
+                        bg-gray-100 text-gray-900 shadow-lg
+                        dark:bg-gray-900 dark:text-white">
+            <SideBarIcon icon={<FaFire size="28" />} />
+            <Divider />
+            <SideBarIcon icon={<BsPlus size="32" />} />
+            <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+            <SideBarIcon icon={<FaPoo size="20" />} />
+            <Divider />
+            <SideBarIcon icon={<BsGearFill size="20" />} />
+        </div>
+    );
+};
+
+const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
+    <div className="sidebar-icon group">
+        {icon}
+
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {text}
+        </span>
+    </div>
+);
+
+const Divider = () => <hr className='sidebar-hr' />;
+
+export default SideBar;
